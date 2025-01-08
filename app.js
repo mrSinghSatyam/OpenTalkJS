6e91a9df5b027fea4c5634d39f1e6ad91ee73762
mrSinghSatyam <satyamsinghrs972518@gmail.com>
2025-01-08 13:14:32 +0530

    Add GenAI - Task 3: Satyam

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 import fs  from 'fs';
-
 import ollama from "ollama";
-
-const q = fs.readFileSync()
+let question;
+let n =3;
+async function ask_ques(question,i) {
+    const response = await ollama.chat({
+        model: "llama3.2:3b",
+        messages: [{ role: "user", content: question}],
+    });
+    let answer = response.message.content;
+    fs.writeFile(`./Answers/a${i}.txt`,answer,(err)=>{
+        if(err){
+            throw err;
+        }
+    });
+}
+for(let i=1;i<=n;i++){
+    question=`./Questions/q${i}.txt`;
+    ask_ques(fs.readFileSync(question, 'utf8',(err)=>{
+        if(err){
+            throw err;
+        }
+    }),i);
+}
